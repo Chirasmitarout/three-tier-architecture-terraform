6bc866aaa8564db9ea4d8f6a275955777d303336
Chirasmita Rout <163724935+Chirasmitarout@users.noreply.github.com>
2026-08-08 17:12:24 +0530

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "twinkuu-bkt"
+    bucket         = "chiruuu-oidc"
     key            = "three-tier-network/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
